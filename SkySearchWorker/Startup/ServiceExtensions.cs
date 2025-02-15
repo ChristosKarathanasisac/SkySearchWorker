7663344b22cea461328c0893b4b7bb1df7deb21e
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2025-02-15 18:02:18 +0200

    Updated the Service Extensions.

--- SkySearchWorker/Startup/ServiceExtensions.cs
+++ SkySearchWorker/Startup/ServiceExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Options;
 using SkySearchWorker.Application.Interfaces;
 using SkySearchWorker.Application.Services;
 using SkySearchWorker.Infrastructure.Configuration;
+using SkySearchWorker.Infrastructure.Interfaces;
 using SkySearchWorker.Infrastructure.Services;
 using System;
 using System.Collections.Generic;
@@ -24,8 +25,11 @@ namespace SkySearchWorker.Startup
                 client.BaseAddress = new Uri(settings.urls.baseUrl);
                 //client.DefaultRequestHeaders.Add("Authorization", $"Bearer {settings.AccessToken}");
             });
-            services.AddSingleton<IHttpClientService, HttpClientService>();
-            services.AddSingleton<IAmadeusAuthenticate, AmadeusAuthenticate>();
+            services.AddSingleton<ICustomHttpClient, CustomHttpClientService>();
+            services.AddSingleton<IAmadeusAuthentication, AmadeusAuthenticationService>();
+            services.AddSingleton<IAmadeusFlightProvider, AmadeusFlightProviderService>();
+            services.AddSingleton<ISkySearchSync, SkySearchSyncService>();
+
             return services;
         }
     }
