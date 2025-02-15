7196cf69516a8a302a99ada48a8c98c48d69bbd0
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2025-02-15 13:50:54 +0200

    Added the the AppSettings.

--- SkySearchWorker/Infrastructure/Configuration/AppSettings.cs
+++ SkySearchWorker/Infrastructure/Configuration/AppSettings.cs
@@ -8,7 +8,20 @@ namespace SkySearchWorker.Infrastructure.Configuration
 {
     public class AppSettings
     {
-        public required string BaseUri { get; set; }
-        public required string AccessToken { get; set; }
+        public required Credentials credentials { get; set; }
+        public required Urls urls { get; set; }
+        public required string amadeusClient { get; set; }
+    }
+    public class Credentials
+    {
+        public required string clientΙd { get; set; }
+        public required string clientSecret { get; set; }
+        public required string grantType { get; set; }
+        public required string accessToken { get; set; }
+    }
+    public class Urls
+    {
+        public required string baseUrl { get; set; }
+        public required string authenticate { get; set; }
     }
 }
