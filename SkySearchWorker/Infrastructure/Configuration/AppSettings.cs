8e9f85e5577fbcfc2af49d0b8213cf8ba3fd4afd
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2025-02-15 10:21:31 +0200

    Added the appsettings class.

---
+++ SkySearchWorker/Infrastructure/Configuration/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySearchWorker.Infrastructure.Configuration
+{
+    public class AppSettings
+    {
+        public required string BaseUri { get; set; }
+        public required string AccessToken { get; set; }
+    }
+}
