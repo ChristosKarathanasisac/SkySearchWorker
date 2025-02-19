b23b75307d83531708351a72ea5deaad84f4e753
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2025-02-19 08:24:19 +0200

    Aded the IExampleHelper and the ExampleHelper.

---
+++ SkySearchWorker/Application/Interfaces/IExampleHelper.cs
@@ -0,0 +1,16 @@
+﻿using SkySearchWorker.Application.DTOs.Amadeus.FlightOffer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySearchWorker.Application.Interfaces
+{
+    public interface IExampleHelper
+    {
+        List<Task<FlightOfferDto>> GetFlightOfferTasks();
+        List<string> GetDateRange(string fromDate, string toDate);
+        List<List<Task<FlightOfferDto>>> GetGroupedFlightOfferTasks();
+    }
+}
---
+++ SkySearchWorker/Application/Services/ExampleHelper.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Options;
+using SkySearchWorker.Application.DTOs.Amadeus.FlightOffer;
+using SkySearchWorker.Application.Interfaces;
+using SkySearchWorker.Infrastructure.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySearchWorker.Application.Services
+{
+    public class ExampleHelper : IExampleHelper
+    {
+        private readonly IAmadeusFlightProvider _amadeusFlightProvider;
+        private readonly AppSettings _appSettings;
+
+        public ExampleHelper(IAmadeusFlightProvider amadeusFlightProvider,
+            IOptions<AppSettings> appSettings)
+        {
+            _amadeusFlightProvider = amadeusFlightProvider;
+            _appSettings = appSettings.Value;
+        }
+        public List<string> GetDateRange(string fromDate, string toDate)
+        {
+            var dateList = new List<string>();
+            var startDate = DateTime.ParseExact(fromDate, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(toDate, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                dateList.Add(date.ToString("yyyy-MM-dd"));
+            }
+            return dateList; 
+        }
+
+        public List<Task<FlightOfferDto>> GetFlightOfferTasks()
+        {
+            var airports = _appSettings.TestData.Airports;
+            var dates = GetDateRange(_appSettings.TestData.FromDate, _appSettings.TestData.ToDate);
+
+            var tasks = (from date in dates
+                         from origin in airports
+                         from destination in airports
+                         where origin != destination
+                         select _amadeusFlightProvider.GetFlightOffers<FlightOfferDto>(new Dictionary<string, string>
+                         {
+                             { "originLocationCode", origin },
+                             { "destinationLocationCode", destination },
+                             { "departureDate", date },
+                             { "adults", "1" },
+                             { "nonStop", "true" },
+                             { "max", "1" }
+                         })).ToList();
+
+            return tasks;
+        }
+
+        public List<List<Task<FlightOfferDto>>> GetGroupedFlightOfferTasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
