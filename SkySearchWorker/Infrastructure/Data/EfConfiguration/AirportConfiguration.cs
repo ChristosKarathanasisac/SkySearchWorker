b240512209d66ee97d3e4968d7cdf5fe6cac016d
ChristosKarathanasisac <christoskarathanasisac@gmail.com>
2025-02-22 17:04:29 +0200

    Added the DataContext and the Configurations

---
+++ SkySearchWorker/Infrastructure/Data/EfConfiguration/AirportConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkySearchWorker.Infrastructure.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySearchWorker.Infrastructure.Data.EfConfiguration
+{
+    public static class AirportConfiguration
+    {
+        public static void Configure(EntityTypeBuilder<Airport> builder)
+        {
+            builder.ToTable("Airport");
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Name).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Code).HasMaxLength(10).IsRequired();
+            builder.Property(a => a.CityCode).HasMaxLength(10).IsRequired();
+            builder.Property(a => a.CountryCode).HasMaxLength(10).IsRequired();
+        }
+    }
+}
---
+++ SkySearchWorker/Infrastructure/Data/EfConfiguration/FlightConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkySearchWorker.Infrastructure.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySearchWorker.Infrastructure.Data.EfConfiguration
+{
+    public static class FlightConfiguration
+    {
+        public static void Configure(EntityTypeBuilder<Flight> builder)
+        {
+            builder.ToTable("Flight");
+
+            builder.HasKey(f => f.Id);
+            builder.Property(f => f.AirlineId).IsRequired();
+            builder.Property(f => f.AirportId).IsRequired();
+            builder.Property(f => f.ArrivalTime).IsRequired();
+            builder.Property(f => f.DepartureTime).IsRequired();
+
+            builder.HasOne(f => f.Airline)
+                   .WithMany(a => a.Flights)
+                   .HasForeignKey(f => f.AirlineId);
+
+            builder.HasOne(f => f.Airport)
+                   .WithMany(a => a.Flights)
+                   .HasForeignKey(f => f.AirportId);
+        }
+    }
+}
